05652efc62a1cc03f990b008a7b561f312b199b6
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-05-27 16:38:14 +0900

    Update and rename 두 수의 차.c to 두 수의 차_.js

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
-    int answer = 0;
-    return answer;
-}
---
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차_.js
@@ -0,0 +1,17 @@
+function solution(num1, num2) {
+    let answer = 0;
+    answer = num1 - num2
+    return answer;
+}
+
+function solution(num1, num2) {
+   let answer = 0;
+    if (-50000 <= num1 && num2 <= 50000) {
+        answer = num1 - num2;
+        return answer;
+    } else{
+        console.log('범위를 초과하였습니다.')
+    }
+}
+
+solution(100, 2)
