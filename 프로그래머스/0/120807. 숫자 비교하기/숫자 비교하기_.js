9b7778b1d44d9c750e97273371d75d0c10b9e9f9
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-05-31 14:19:55 +0900

    Create 숫자 비교하기_.js

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기_.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    let answer = 0;
+    if(num1 === num2){
+        return answer = 1;
+    } else if(num1 !== num2){
+        return answer = -1;
+
+    }
+    return answer;
+}
+solution(2,3);
+solution(11,11);
