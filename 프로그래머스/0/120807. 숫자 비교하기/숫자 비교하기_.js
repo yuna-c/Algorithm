44feb43a94106aef1ca3dd272570d1cf42ac68df
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-05-31 14:21:29 +0900

    Update 숫자 비교하기_.js

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기_.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기_.js
@@ -1,10 +1,9 @@
 function solution(num1, num2) {
     let answer = 0;
     if(num1 === num2){
-        return answer = 1;
+       answer = 1;
     } else if(num1 !== num2){
-        return answer = -1;
-
+        answer = -1;
     }
     return answer;
 }
