a009a301c16b8b46f111d43b2c5cc5cbdc5e1f10
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-07 17:43:53 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,6 +1,6 @@
 function solution(num1, num2) {
     let answer = 0;
-    if(0 <= num1 && num2 <= 100){
+    if(0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100){
         answer = Math.floor(num1 / num2);
         return answer;
     } else {
