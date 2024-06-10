9d8dc3631f28399d646131f9824efd59b0defc73
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-10 10:36:27 +0900

    [level 0] Title: 두 수의 차, Time: 0.01 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.js
@@ -1,5 +1,11 @@
 function solution(num1, num2) {
-    let answer = 0;
-    answer = num1 - num2
-    return answer;
+   let answer = 0;
+    if (-50000 <= num1 && num1<= 50000 && -50000 <= num2 && num2 <= 50000) {
+        answer = num1 - num2;
+        return answer;
+    } else{
+        console.log('범위를 초과하였습니다.')
+    }
 }
+
+solution(100, 2)
