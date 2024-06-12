fb669bc195a0c1ef81a232df94a5e33ab6e9c5a3
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-12 16:36:42 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,3 +1,12 @@
+// var vegetables = ["설탕당근", "감자"];
+// var moreVegs = ["셀러리", "홍당무"];
+
+// // 첫번째 배열에 두번째 배열을 합친다.
+// // vegetables.push('셀러리', '홍당무'); 하는 것과 동일하다.
+// Array.prototype.push.apply(vegetables, moreVegs);
+
+// console.log(vegetables); // ['설탕당근', '감자', '셀러리', '홍당무']
+
 function solution(x, n) {
     let answer = [];
     for(let i = 1; i <= n; i++){
