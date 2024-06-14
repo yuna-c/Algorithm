e32ae636070f55953981d92b470747072d85910c
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-14 14:57:10 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 2.49 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,11 +1,12 @@
 function solution(n) {
     let answer = 0;
-    let math = Math.sqrt(n);
+    let sqrt = Math.sqrt(n); //제곱식 구하기
+    console.log(sqrt);
     
-    if(math % 1 === 0){ answer += Math.pow(math+1,2); }
-    else{ answer = -1; }
-    
+    if(sqrt % 1 === 0){ answer += Math.pow(sqrt+1,2); } //제곱근일 경우
+    else{ answer = -1; } //제곱근이 아닐 경우
     console.log(answer);
+    
     return answer;
 }
 
