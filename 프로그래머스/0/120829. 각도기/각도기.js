33a61c79901fb2a403f3d38556773bed1a48a44f
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-25 14:23:35 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,13 +1,4 @@
 function solution(angle) {
-    let answer = 0;
-    if(angle > 0 && angle < 90){
-        answer = 1;
-    } else if(angle === 90){
-        answer = 2;
-    } else if(angle > 90 && angle < 180){
-        answer = 3;
-    } else if(angle === 180){
-        answer = 4;
-    }
-    return answer;
+    // return [0, 90, 91, 180].filter(x => angle>=x).length;
+     return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
 }
