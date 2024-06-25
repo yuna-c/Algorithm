dcb581fa0cc3980cbba705985b92dad4886cc69d
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-25 15:21:52 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 25일 15:21:00
+2024년 06월 25일 15:21:51
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,7 +1,8 @@
 function solution(absolutes, signs) {
-    let answer = 0;
-    for(let i = 0; i < absolutes.length; i++){
-        signs[i] ? answer += absolutes[i] : answer -= absolutes[i]
-    }
-    return answer;
+    // let answer = 0;
+    // for(let i = 0; i < absolutes.length; i++){
+    //     signs[i] ? answer += absolutes[i] : answer -= absolutes[i]
+    // }
+    // return answer;
+    return absolutes.reduce((acc, val, i) => acc + (val * (signs[i] ? 1 : -1)), 0);
 }
