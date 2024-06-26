fc6a0038b818138eb75fe8b4d9b403c21705ef9f
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-06-26 15:27:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -9,3 +9,8 @@ function solution(n) {
     // n을 2로나눈 몫 만큼 곱하고 나머지가 있으면 '수' 더해라.
     // return ("수박").repeat(n/2) + ((n%2) ? '수' : '');
 }
+
+// var waterMelon = n =>'수박'.repeat(n/2) + (n%2 === 1 ? '수' : '');
+
+// // 실행을 위한 테스트코드입니다.
+// console.log("n이 28인 경우: "+ waterMelon(28))
