8e781b0ecc71562bd0d7fec4688d8158ebddd097
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-07-10 16:41:04 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,9 +1,9 @@
 function solution(s) {
     let answer = true;
     if (s.length === 4 || s.length === 6) {
-    for (let i = 0; i < s.length; i++) {
-      if (isNaN(s[i])) return !answer;
-    }
+        for (let i = 0; i < s.length; i++) {
+          if (isNaN(s[i])) return !answer;
+        }
     return answer;
   }
   return !answer;
