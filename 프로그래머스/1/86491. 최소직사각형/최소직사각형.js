c09f1da45a8e1e36022a3b19123ac7848aac455f
yuna-c <65221477+yuna-c@users.noreply.github.com>
2024-07-10 16:28:07 +0900

    [level 1] Title: 최소직사각형, Time: 8.58 ms, Memory: 41.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,9 @@
+function solution(sizes) {
+    let answer = [0,0]
+    for(let i = 0 ; i < sizes.length;i++){
+        let [w,h] = sizes[i].sort((a,b)=>b-a)
+        if(w > answer[0]){ answer[0] = w }
+        if(h > answer[1]){ answer[1] = h }
+    }
+    return answer[0]*answer[1]
+}
