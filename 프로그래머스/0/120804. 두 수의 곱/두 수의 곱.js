0aa439fb94afe1f23392609d52e64e814c55af18
yuna <65221477+yuna-c@users.noreply.github.com>
2024-07-22 13:59:54 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.js
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.js
@@ -1,12 +1,9 @@
-function solution(num1, num2) {
-    let answer = 0;
-    if(0 <= num1 && num2 <= 100){
-        answer = num1 * num2;
-        return answer;
-    } else {
-        alert("❌");
-    }
+const solution = (num1, num2) => num1 * num2
+// function solution(num1, num2) {
+//     let answer = 0;
+//     answer = num1 * num2;
+//     return answer;
     
-}
+// }
 solution(3,4);
 solution(27,19);
