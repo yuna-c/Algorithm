77c91a7f50bbda0243d8c7d5e1efcf16b96aa92b
yuna <65221477+yuna-c@users.noreply.github.com>
2024-07-22 13:47:13 +0900

    [level 1] Title: 직사각형 별찍기, Time: 69.46 ms, Memory: 32.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -1,9 +1,10 @@
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
-    const n = data.split(" ");
+    const n = data.split(" "); // data를 "" 기준으로 나눈다
     const a = Number(n[0]), 
           b = Number(n[1]);
-    // console.log(a);
-    // console.log(b);
-    console.log(('*'.repeat(a) + '\n').repeat(b));
+    // console.log(a); 5
+    // console.log(b); 3
+    console.log(('*'.repeat(a) + '\n').repeat(b)); 
+    // *을 a의 갯수만큼 반복하고, '\enter' 처리 한후, 다시 b갯수 만큼 반복한다
 });
