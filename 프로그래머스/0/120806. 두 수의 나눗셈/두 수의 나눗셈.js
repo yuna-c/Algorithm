5cc416cce469f088f91f0b14bcd29c64416888f2
yuna <65221477+yuna-c@users.noreply.github.com>
2024-07-22 14:06:55 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,6 +1,7 @@
-function solution(num1, num2) {
-    let answer = 0;
-    answer = Math.floor((num1 / num2) * 1000);
-    return answer;
-}
+const solution =(num1, num2) => answer = Math.floor((num1 / num2) * 1000);
 
+// function solution(num1, num2) {
+//     let answer = 0;
+//     answer = Math.floor((num1 / num2) * 1000);
+//     return answer;
+// }
