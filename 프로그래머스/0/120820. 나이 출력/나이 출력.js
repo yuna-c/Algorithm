464b2a8ac85dbdc38dad690ae1f5fa37567f55e3
yuna <65221477+yuna-c@users.noreply.github.com>
2024-07-22 13:52:17 +0900

    [level 0] Title: 나이 출력, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.js
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -1,9 +1,3 @@
 function solution(age) {
-    let now = 2022;
-    let answer = 0;
-    if(age > 0 && age <= 120)
-    {
-       answer = now - age + 1 ;
-    }
-    return answer;
+    return answer = 2022 - age + 1;
 }
