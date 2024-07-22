0a7dd1fbebfdb2b245fb2d2bf24ed8cccee192ec
yuna <65221477+yuna-c@users.noreply.github.com>
2024-07-22 12:22:25 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.24 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,20 @@
+function solution(food) {
+    let answer = '';
+    // [1(물), 3, 4, 6] 0[i]은 물 
+    // console.log(food); 숫자 확인
+    
+    // 반복 돌리기(for, map, reduce)
+    // String(n)명시적 형변환,(n+"")암시적 형변환
+    // repeat(); '문자열'을 주어진 횟수만큼 반복해 붙임 
+    // reverse(); 뒤집기
+    // join(); 합치기
+    food.map((e,i) => {
+        if(e > 1){ // 물은 빼야 하기 때문에 물이 1보다 작은 경우의 조건으로 필터링
+            answer += String(i).repeat(e / 2); 
+        }
+    });
+    return answer = `${answer}0${[...answer].reverse().join("")}`;
+    // [...answer] 스프레드 연산자를 통해 얕은 복사(원본 안 건들게) 후 진행
+}
+console.log(solution([1,3,4,6]));
+console.log(solution([1,7,1,2]));
