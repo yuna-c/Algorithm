b77822c2729542551c5c7730cc988947a44496f5
yuna <65221477+yuna-c@users.noreply.github.com>
2024-07-25 15:03:39 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 7.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,8 +1,37 @@
 function solution(strings, n) {
-    // 정렬
-    return strings.sort((a, b) => {
-      if (a[n] > b[n]) return 1;
-      else if (a[n] < b[n]) return - 1;
-      else return a > b ? 1 : - 1;
-    });
-  }
+    let answer = [];
+    console.log(strings);
+    console.log(answer);
+    console.log(n);
+    
+    for(let i = 0; i < strings.length; i++){
+        
+        console.log('1',strings[i], '2',strings[i][n]);
+        strings[i] = strings[i][n] + strings[i];
+        // answer.push(strings[i]);
+        
+    }
+    
+    strings.sort();
+    
+    for(let j = 0; j < strings.length; j++){
+        // console.log('3',strings[j])
+        strings[j] = strings[j].replace(strings[j][0],'');
+        answer.push(strings[j]);
+        
+    }
+    return answer;
+}
+
+// function solution(strings, n) {
+//     return strings.sort((a, b) => {
+//         // a[n]이 b[n]보다 크면 1을 반환하고 // 내림차순
+//       if (a[n] > b[n]) return 1;
+ 
+//         // a[n]이 b[n]보다 작으면 -1을 반환하고 // 오름차순
+//       else if (a[n] < b[n]) return -1;
+ 
+//         // a[n]이 b[n]과 같으면 사전순으로 정렬한다.
+//       else return a > b ? 1 : -1;
+//     });
+//   }
