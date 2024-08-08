9bfe12d4739d8f660cb2a537c931f9e30ab5e0a7
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-08 20:01:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/README.md
+++ 프로그래머스/0/120822. 문자열 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 뒤집기 - 120822 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120822) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120822?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.03 ms
+메모리: 33.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 08일 17:16:34
+2024년 08월 08일 20:01:01
 
 ### 문제 설명
 
--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,9 +1,13 @@
-// function solution(my_string) {
-//     let reverse = '';
-//     console.log(my_string); //문자열
-//     // reverse = [...my_string].reverse().join('');
-//     reverse = my_string.split('').reverse().join('');
-//     return reverse;
-// }
-
-const solution = (my_string) => [...my_string].reverse().join('');
+function solution(my_string) {
+    let reverse = '';
+    // solution 1 : for()
+    for(let i = 1; i <= my_string.length; i++){
+        reverse += my_string[my_string.length-i]
+    }
+    return reverse;
+    // solution 2 : reverse()
+    // return reverse = my_string.split('').reverse().join('');
+    
+    // solution 3 : spead
+    // return reverse = [...my_string].reverse().join('');
+}
