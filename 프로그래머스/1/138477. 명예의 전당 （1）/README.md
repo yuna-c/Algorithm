0d417880d115efae7380fdf836f4f594857b1353
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-08 16:41:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 19.38 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
 
 ### 성능 요약
 
-메모리: 36.5 MB, 시간: 5.74 ms
+메모리: 36.2 MB, 시간: 19.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 08일 16:27:42
+2024년 08월 08일 16:41:06
 
 ### 문제 설명
 
--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,21 +1,16 @@
 function solution(k, score) {
-    let answer = []; // 최하점
-    let award = [];  // 명예의 전당
-    // for of
-    for(let s of score){
-        // console.log(`s번째 스코어의 값 : ${s}`); // [10, 100, 20, 150, 1, 100, 200]
-        // console.log(`배열의 길이(idx) : ${award.length}`) // 0,1,2,3,4,5,6
-        award.push(s);
-        // console.log(award.sort((a,b)=> b-a)); // 내림차순 정렬
-        award.sort((a,b)=> b-a);
-        if(award.length === k+1){
-            award.pop();
-        }
-        answer.push(award[award.length -1])
-    }
-    // console.log('사람수',k);
-    // console.log('빈배열',answer);
-    // console.log('최고 점수',score);
-    // console.log('최고 점수의 길이',score.length);
-    return answer;
+  let answer = [],
+      award = [];
+    
+  // score 전체를 탐색하면서 
+  for (const s of score) {
+    // award에 score을 push하고 오름차순으로 정렬
+    award.push(s);
+    award.sort((a, b) => a - b);
+    // award의 길이가 k보다 커지면 제일 작은 원소인 맨 앞의 원소를 shift로 제거
+    if (award.length > k) award.shift();
+    // 제일 첫번째 원소를 넣는다
+    answer.push(award[0])
+  }
+  return answer
 }
