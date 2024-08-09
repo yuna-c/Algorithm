ca7f44878130066713fa2f4f515c63706cc86e92
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-09 09:45:51 +0900

    [level 1] Title: 2016년, Time: 0.20 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,5 +1,6 @@
 function solution(a, b) {
-    const day = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
-    let date = new Date (`2016-${a}-${b}`).getDay();
-    return day[date];
+    let week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    let date = new Date (`2016-${a}-${b}`);
+    let day = date.getDay();
+    return week[day];
 }
