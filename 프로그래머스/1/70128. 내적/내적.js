c3e863219da6f5f631e18ac97faa24620c38c730
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-13 09:58:10 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,9 +1,9 @@
-function solution(a, b) {
-    // let answer = 0;
-    // for(let i=0; i<a.length; i++){
-    //     answer += a[i]*b[i];
-    // }
-    // return answer;
-    
-    return a.reduce((acc,cur,i) => acc += a[i] * b[i], 0);
-}
+// function solution(a, b) {
+//     let answer = 0;
+//     for(let i=0; i<a.length; i++){
+//         answer += a[i]*b[i];
+//     }
+//     return answer;
+// }
+
+const solution = (a,b) => a.reduce((acc,cur,i) => acc += a[i] * b[i], 0);
