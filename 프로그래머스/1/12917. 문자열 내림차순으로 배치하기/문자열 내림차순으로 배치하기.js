d53c3a1d100e14e7e3c9b0182daa6e722bc43fcb
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-15 22:27:36 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,5 +1,9 @@
-function solution(s) {
-    let answer = "";
-    answer = s.split("").sort().reverse().join("");
-    return answer;
-}
+// function solution(s) {
+// //     let answer = "";
+// //     answer = s.split("").sort().reverse().join("");
+// //     return answer;
+//     return s.split("").sort().reverse().join("");
+//     // s.문자열 나눠서 배열화().정렬().뒤집고().배열 연결 후 문자화();
+// }
+
+const solution =(s)=> s.split("").sort().reverse().join("");
