365229ca1cf31d29efc9f771af2be5ab7ed06a13
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-16 09:20:04 +0900

    [level 0] Title: n의 배수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/README.md
+++ 프로그래머스/0/181937. n의 배수/README.md
@@ -1,10 +1,10 @@
 # [level 0] n의 배수 - 181937 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181937?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181937) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.5 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 16일 09:17:45
+2024년 08월 16일 09:20:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181937. n의 배수/n의 배수.js
+++ 프로그래머스/0/181937. n의 배수/n의 배수.js
@@ -1,10 +1,11 @@
-function solution(num, n) {
-    let answer = 0;
-    if(num % n === 0) {
-      return  answer = 1;
-    } else {
-      return answer= 0;
-    }
-  
-    return answer;
-}
+// function solution(num, n) {
+//     let answer = 0;
+//     if(num % n === 0) {
+//       return answer = 1;
+//     } else {
+//       return answer= 0;
+//     }
+//     return answer;
+// }
+
+const solution = (num, n)=> num % n === 0 ? 1 : 0;
