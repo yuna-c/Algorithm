cbe2395fd0b0294d6a79ead22b7616b78a99ab8a
yuna <65221477+yuna-c@users.noreply.github.com>
2024-08-16 10:30:37 +0900

    [level 1] Title: 모의고사, Time: 4.63 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,27 @@
+function solution(answers) {
+    // 각 수포자의 패턴
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ]
+    
+    // 수포자의 답을 저장할 빈 배열
+    const scores = [0,0,0]
+    
+    // 정답과 패턴 비교하여 점수 계산
+    answers.forEach((answer, idx) => {
+        patterns.forEach((pattern, index) => {
+          if(answer === pattern[idx % pattern.length]){
+              scores[index]++;
+          }
+        });
+    })
+    
+    // 가장 높은 점수 찾기
+    const maxScore = Math.max(...scores);
+    
+    // 가장 높은 점수를 받은 수포자의 번호를 반환
+    return scores.map((score, i) => score === maxScore ? i + 1 : null ).filter(val => val !== null);
+    // 오름차순 return
+}
