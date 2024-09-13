2b53df707d1491d60d8a85ee7bb2e31928d90586
yuna <65221477+yuna-c@users.noreply.github.com>
2024-09-13 10:55:11 +0900

    [level 1] Title: 체육복, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,25 +1,27 @@
 function solution(n, lost, reserve) {
-
-		// 여벌없이 분실한 학생
-    let realLost = lost.filter(l => !reserve.includes(l)).sort((a, b) => a - b);
-		// 분실없이 여벌이 있는 학생
-    let realReserve = reserve.filter(r => !lost.includes(r)).sort((a, b) => a - b);
+    let 도난노여분 = lost.filter(분실 => !reserve.includes(분실)).sort((a, b) => a - b);
+    let 노도난여분 = reserve.filter(여분 => !lost.includes(여분)).sort((a, b) => a - b);
     
-		// 체육복을 빌릴 수 있는 학생 수
-    let answer = n - realLost.length;
+    let 답 = n - 도난노여분.length;
 
-    for (let i = 0; i < realLost.length; i++) {
-        let lostNum = realLost[i];
+    for (let i = 0; i < 도난노여분.length; i++) {
+        let 분실번호 = 도난노여분[i];
       
-        for (let j = 0; j < realReserve.length; j++) {
-            let reserveNum = realReserve[j];
-            if (reserveNum === lostNum - 1 || reserveNum === lostNum + 1) {
-              answer += 1;
-              realReserve[j] = -1; 
-              break; 
+        for (let j = 0; j < 노도난여분.length; j++) {
+            let 여분번호 = 노도난여분[j];
+            if (여분번호 === 분실번호 - 1 || 여분번호 === 분실번호 + 1) {
+                답 += 1;
+                // 빌려준 후 다시 사용되지 않도록 처리
+                노도난여분[j] = -1;
+                break; 
             }
         }
-      }
+    }
 
-    return answer;
+    return 답;
 }
+
+// 테스트 케이스
+console.log(solution(5, [2, 4], [1, 3, 5])); // 5
+console.log(solution(5, [2, 4], [3])); // 4
+console.log(solution(3, [3], [1])); // 2
