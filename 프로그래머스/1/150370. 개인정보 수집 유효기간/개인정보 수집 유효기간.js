9b74ea6849853787b1a09c2ede63285890b39e61
yuna <65221477+yuna-c@users.noreply.github.com>
2024-10-23 10:02:15 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.86 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,11 +1,9 @@
 function solution(today, terms, privacies) {
-    // 날짜를 일 단위로 변환하는 함수 
     const toDays = (date) => {
         const [year, month, day] = date.split('.').map(Number);
         return year * 12 * 28 + month * 28 + day;
     }
     
-    // today를 일 단위로 변환
     const todayDays = toDays(today);
     
     // 약관 종류에 따른 유효기간을 저장할 딕셔너리
@@ -15,7 +13,6 @@ function solution(today, terms, privacies) {
         termDict[key] = Number(value);
     })
     
-    // 파기해야 할 개인정보 번호를 저장할 배열
     let result = [];
     
     privacies.forEach((privacy, index) => {
