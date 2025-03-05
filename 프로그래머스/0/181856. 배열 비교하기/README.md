e4058515ed6f8dcd02f79ca979e154990561b5dd
yuna <65221477+yuna-c@users.noreply.github.com>
2025-03-05 20:37:57 +0900

    [level 0] Title: 배열 비교하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/README.md
+++ 프로그래머스/0/181856. 배열 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 비교하기 - 181856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.10 ms
+메모리: 33.5 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 20:20:48
+2025년 03월 05일 20:37:56
 
 ### 문제 설명
 
--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -1,11 +1,22 @@
 function solution(arr1, arr2) {
-    if(arr1.length > arr2.length) return 1;
-    if(arr1.length < arr2.length) return -1;
-    
-    let sum1 = arr1.reduce((a,b) => a + b, 0);
-    let sum2 = arr2.reduce((a,b) => a + b, 0);
-    
-    if(sum1 > sum2) return 1;
-    if(sum1 < sum2 ) return -1;
-    return 0;
+    let len1 = arr1.length;
+    let len2 = arr2.length; 
+    let sum1 = arr1.reduce((a, b) => a + b, 0); 
+    let sum2 = arr2.reduce((a, b) => a + b, 0); 
+
+    return len1 !== len2 ? (len1 > len2 ? 1 : -1) 
+         : sum1 !== sum2 ? (sum1 > sum2 ? 1 : -1) 
+         : 0;
 }
+
+// function solution(arr1, arr2) {
+//     if(arr1.length > arr2.length) return 1;
+//     if(arr1.length < arr2.length) return -1;
+    
+//     let sum1 = arr1.reduce((a,b) => a + b, 0);
+//     let sum2 = arr2.reduce((a,b) => a + b, 0);
+    
+//     if(sum1 > sum2) return 1;
+//     if(sum1 < sum2 ) return -1;
+//     return 0;
+// }
