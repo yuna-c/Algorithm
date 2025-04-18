7dd4f4653124058c73d5fe35c17f9571e45562fc
yuna <65221477+yuna-c@users.noreply.github.com>
2025-04-18 18:01:22 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    let answer = 0;
+    
+    if(n % 2 === 1){
+        for(let i = 1; i <= n; i += 2){
+            answer += i;
+        }
+    } else {
+        for(let i = 2; i <= n; i += 2){
+            answer += i * i;
+        }
+    }
+    return answer;
+}
+
+console.log(solution(4))
