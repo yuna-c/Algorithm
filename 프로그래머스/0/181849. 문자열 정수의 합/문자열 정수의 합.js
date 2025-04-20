11283fb94646cc9daa689e0005c09c42759471cf
yuna <65221477+yuna-c@users.noreply.github.com>
2025-04-20 22:11:14 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.js
@@ -1,7 +1,11 @@
+// function solution(num_str) {
+//     let answer = 0;
+//     for(let i = 0; i < num_str.length; i ++){
+//         answer += Number(num_str[i])
+//     }
+//     return answer;
+// }
+
 function solution(num_str) {
-    let answer = 0;
-    for(let i = 0; i < num_str.length; i ++){
-        answer += Number(num_str[i])
-    }
-    return answer;
+    return num_str.split('').reduce((sum,num)=> sum + Number(num), 0);
 }
