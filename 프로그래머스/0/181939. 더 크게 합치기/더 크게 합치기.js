1572557bb3d78dcd1b949007ccfc0db9f3717fc4
yuna <65221477+yuna-c@users.noreply.github.com>
2025-04-22 16:03:41 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,5 +1,7 @@
-function solution(a, b) {
-    let ab = Number(`${a}${b}`);
-    let ba = Number(`${b}${a}`);
-    return ab >= ba ? ab : ba;
-}
+// function solution(a, b) {
+//     let ab = Number(`${a}${b}`);
+//     let ba = Number(`${b}${a}`);
+//     return ab >= ba ? ab : ba;
+// }
+
+const solution = (a, b) => Math.max(Number(`${a}${b}`), Number(`${b}${a}`));
