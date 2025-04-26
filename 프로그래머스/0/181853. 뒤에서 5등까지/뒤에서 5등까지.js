d9bb17881803db50be2de29bdbb8fd81cd9cd21a
yuna <65221477+yuna-c@users.noreply.github.com>
2025-04-26 22:41:44 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -5,9 +5,14 @@
 
 // const solution = (num_list) => num_list.sort((a, b)=> a - b).slice(0, 5) 
 
-const solution = (num_list) => {
-    let answer = [];
-    num_list.sort((a, b) => a - b);
-    for (let i=0; i<5; i++) {answer.push(num_list[i]);}
+// const solution = (num_list) => {
+//     let answer = [];
+//     num_list.sort((a, b) => a - b);
+//     for (let i = 0; i < 5; i++) {answer.push(num_list[i])}
+//     return answer;
+// }
+
+function solution(num_list) {
+    let answer = [...num_list].sort((a, b) => a - b).slice(0, 5);
     return answer;
 }
