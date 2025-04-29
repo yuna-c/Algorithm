ae6b396595ebedf48add915f71878badae8dbc6f
yuna <65221477+yuna-c@users.noreply.github.com>
2025-04-29 19:21:53 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,5 +1,10 @@
+// function solution(n, k) {
+//     let free = Math.floor(n/10);
+//     let bill = (n * 12000) + ((k - free) * 2000)
+//     return bill;
+// }
+
 function solution(n, k) {
-    let free = Math.floor(n/10);
-    let bill = (n * 12000) + ((k - free) * 2000)
-    return bill;
+    k -= Math.floor(n / 10);
+    return (n * 12000) + (k * 2000);
 }
