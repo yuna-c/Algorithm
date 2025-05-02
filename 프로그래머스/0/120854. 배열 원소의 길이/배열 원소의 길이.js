cb07988b21a16943c07e0f4568d52b1106562a65
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-02 20:27:19 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,7 +1,12 @@
+// function solution(strlist) {
+//     let answer = [];
+//     for(let i = 0; i < strlist.length; i++){
+//         answer.push(strlist[i].length)
+//     }
+//     return answer;
+// }
+
 function solution(strlist) {
-    let answer = [];
-    for(let i = 0; i < strlist.length; i++){
-        answer.push(strlist[i].length)
-    }
+    let answer = strlist.map((str) => str.length);
     return answer;
 }
