7ac99bad2aeedcd3720ad01d2b01be3b4cbba7d9
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-03 17:55:07 +0900

    [level 0] Title: 배열의 유사도, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,15 @@
 function solution(s1, s2) {
-    let answer = 0;
-    for(let i = 0; i < s1.length; i++){
-        if(s2.includes(s1[i])){
-            answer++
-        }
-    }
+    let answer = s1.filter((el)=> s2.includes(el)).length;
     return answer;
 }
+
+// function solution(s1, s2) {
+//     let answer = 0;
+//     for(let i = 0; i < s1.length; i++){
+//         if(s2.includes(s1[i])){
+//             answer++
+//         }
+//     }
+//     return answer;
+// }
+
