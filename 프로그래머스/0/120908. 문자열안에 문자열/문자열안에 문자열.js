7794f9374158e4474d94392d848f00b9fa364afe
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-07 18:40:43 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -7,7 +7,9 @@
 //     return answer;
 // }
 
-function solution(str1, str2) {
-    let answer = str1.includes(str2) ? 1 : 2;
-    return answer;
-}
+// function solution(str1, str2) {
+//     let answer = str1.includes(str2) ? 1 : 2;
+//     return answer;
+// }
+
+const solution = (str1, str2) => str1.includes(str2) ? 1 : 2
