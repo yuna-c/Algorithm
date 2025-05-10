49a3c0b24ace579925f15491c6146ddcf64ffbf2
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-10 16:53:23 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,4 +1,8 @@
+// function solution(my_string, letter) {
+//     let answer = my_string.replaceAll(letter, '');
+//     return answer;
+// }
+
 function solution(my_string, letter) {
-    let answer = my_string.replaceAll(letter, '');
-    return answer;
+    return my_string.split(letter).join('');
 }
