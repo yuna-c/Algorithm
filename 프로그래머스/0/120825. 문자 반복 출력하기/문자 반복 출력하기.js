93bd58071014ce7b637fd336365a54d791770dff
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-16 22:08:30 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,6 +1,16 @@
 // function solution(my_string, n) {
+//     let answer = '';
+//     for(let i = 0; i < my_string.length; i++){
+//         for(let j = 0; j < n; j++){
+//             answer += my_string[i];
+//         }
+//     }
+//     return answer;
+// }
+
+// function solution(my_string, n) {
 //     let answer = my_string.split('').map((el)=> el.repeat(n)).join('')    
 //     return answer;
 // }
 
-const solution = (my_string, n) => my_string.split('').map((el)=> el.repeat(n)).join('')    
+const solution = (my_string, n) => my_string.split('').map((el)=> el.repeat(n)).join('')    ;
