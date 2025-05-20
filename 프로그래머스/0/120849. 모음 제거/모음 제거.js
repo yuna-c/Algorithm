b4ed32bab68b62227c0ef83cd9b3cfe15a9bafcf
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-20 22:14:32 +0900

    [level 0] Title: 모음 제거, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,11 +1,13 @@
-function solution(my_string) {
-    let answer = '';
-    let vowels = ['a','e','i','o','u'];
+// function solution(my_string) {
+//     let answer = '';
+//     let vowels = ['a','e','i','o','u'];
     
-    for(let i = 0; i < my_string.length; i++){
-        if(!vowels.includes(my_string[i])){
-            answer += my_string[i]
-        }
-    }
-    return answer;
-}
+//     for(let i = 0; i < my_string.length; i++){
+//         if(!vowels.includes(my_string[i])){
+//             answer += my_string[i]
+//         }
+//     }
+//     return answer;
+// }
+
+const solution = (my_string) => my_string.replace(/[aeiou]/g, '');
