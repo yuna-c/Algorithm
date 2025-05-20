d1c691e3736c3bab375faae754c58d9f676ddf41
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-20 22:13:12 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let answer = '';
+    let vowels = ['a','e','i','o','u'];
+    
+    for(let i = 0; i < my_string.length; i++){
+        if(!vowels.includes(my_string[i])){
+            answer += my_string[i]
+        }
+    }
+    return answer;
+}
