8277b9c6ac28fb8c97d24b8e465773eba2384b5d
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-20 21:06:21 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -1,6 +1,6 @@
 function solution(n, t) {
     // let answer = n * Math.pow(2, t);
     // let answer = n * 2 ** t;
-    let answer = n << t;
+    let answer = n << t; // 비트 연산자
     return answer;
 }
