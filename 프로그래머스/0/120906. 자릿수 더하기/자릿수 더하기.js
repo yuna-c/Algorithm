0a368035819790d4905216d9542a35ad0deef9e6
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-21 20:51:16 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,4 +1,9 @@
 function solution(n) {
-    let answer = n.toString().split('').map(Number).reduce((acc, num) => acc + num, 0);
+    let answer = 0;
+    // let answer = n.toString().split('').map(Number).reduce((acc, num) => acc + num, 0);
+    let str = n.toString();
+    for(let i = 0; i < str.length; i++){
+        answer += parseInt(str[i])
+    }
     return answer;
 }
