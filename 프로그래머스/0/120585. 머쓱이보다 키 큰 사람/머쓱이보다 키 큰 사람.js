11eeaf403a97af2fd8e75fbefcda047788ab891a
yuna <65221477+yuna-c@users.noreply.github.com>
2025-05-22 19:57:24 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,14 @@
+// function solution(array, height) {
+//     let answer = 0;
+//     for(let i = 0; i < array.length; i++) {
+//         if(array[i] > height) {
+//             answer++
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(array, height) {
-    let answer = 0;
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] > height) {
-            answer++
-        }
-    }
+    let answer = array.filter((el) => el > height).length;
     return answer;
 }
