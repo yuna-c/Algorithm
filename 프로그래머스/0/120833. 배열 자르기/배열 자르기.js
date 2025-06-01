d8f401083543b8d818d575da59a3446795ea3dcb
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-01 20:03:47 +0900

    [level 0] Title: 배열 자르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,4 +1,10 @@
 function solution(numbers, num1, num2) {
-    let answer = numbers.slice(num1, num2+1);
+    let answer = [];
+     for (let i = num1; i < num2+1; i++) {
+        answer.push(numbers[i])
+    }
+    
+    // let answer = numbers.slice(num1, num2+1);
+
     return answer;
 }
