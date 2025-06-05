38e9a86b7f30fc955d1bfeef6ce93296a30c581c
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-06 01:46:08 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,7 +1,11 @@
+// function solution(dot) {
+//     const [x,y] = dot;
+//     if(x > 0 && y > 0) return 1;
+//     if(x < 0 && y > 0) return 2;
+//     if(x < 0 && y < 0) return 3;
+//     if(x > 0 && y < 0) return 4;
+// }
+
 function solution(dot) {
-    const [x,y] = dot;
-    if(x > 0 && y > 0) return 1;
-    if(x < 0 && y > 0) return 2;
-    if(x < 0 && y < 0) return 3;
-    if(x > 0 && y < 0) return 4;
+    return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] > 0 ? 2 : 3;
 }
