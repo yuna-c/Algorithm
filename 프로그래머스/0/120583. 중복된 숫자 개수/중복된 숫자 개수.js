9203d9da9e5dfa6fcf1b697ca641693fe3d94dd1
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-10 19:34:50 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,9 +1,14 @@
-function solution(array, n) {
-    let answer = [];
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === n){
-            answer.push(array[i])
-        }
-    }
-    return answer.length;
+// function solution(array, n) {
+//     let answer = [];
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] === n){
+//             answer.push(array[i])
+//         }
+//     }
+//     return answer.length;
+// }
+
+const solution = (array, n) => { 
+    const newArr = array.filter((item)=> item ===n)
+    return newArr.length
 }
