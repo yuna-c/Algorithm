9985b12eafe44c56cb24040c015af22c24539aa2
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-13 18:46:14 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,9 +1,12 @@
-function solution(n) {
-    let answer = [];
-    for(let i = 0; i <= n; i++){
-        if(i % 2 === 1){
-            answer.push(i)
-        }
-    }
-    return answer;
-}
+// function solution(n) {
+//     let answer = [];
+//     for(let i = 0; i <= n; i++){
+//         if(i % 2 === 1){
+//             answer.push(i)
+//         }
+//     }
+//     return answer;
+// }
+
+// 길이가 n인 배열
+const solution = (n) => Array.from({length: n}, (_, i) => i + 1).filter(num => num % 2 === 1)
