486e27368e76356f2e0b9bf6762c33b080e69d3e
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-15 19:00:39 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,6 +1,8 @@
-function solution(price) {
-    if(price >= 500000) price = price * 0.8
-    else if(price >= 300000) price = price * 0.9
-    else if(price >= 100000) price = price * 0.95
-    return Math.floor(price);
-}
+// function solution(price) {
+//     if(price >= 500000) price = price * 0.8
+//     else if(price >= 300000) price = price * 0.9
+//     else if(price >= 100000) price = price * 0.95
+//     return Math.floor(price);
+// }
+
+const solution = (price) => Math.floor(price >= 500000 ? price * 0.8 : price >= 300000 ? price * 0.9 : price >= 100000 ? price * 0.95 : price)
