394b0ecbca8b6ac2937f7aa78489b65e7f384a14
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-21 01:40:07 +0900

    [level 0] Title: 주사위의 개수, Time: 0.07 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,13 @@
 function solution(box, n) {
-    const box0 = Math.floor(box[0]/n);
-    const box1 = Math.floor(box[1]/n);
-    const box2 = Math.floor(box[2]/n);
-    const answer = box0*box1*box2
+    let [width, length, height] = box;
+    let answer = Math.floor(width /n) * Math.floor(length /n) * Math.floor(height /n);
     return answer;
 }
+
+// function solution(box, n) {
+//     const box0 = Math.floor(box[0]/n);
+//     const box1 = Math.floor(box[1]/n);
+//     const box2 = Math.floor(box[2]/n);
+//     const answer = box0*box1*box2
+//     return answer;
+// }
