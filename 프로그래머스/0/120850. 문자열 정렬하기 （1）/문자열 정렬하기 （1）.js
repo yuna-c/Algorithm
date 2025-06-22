23acf5af8b8108865f10322d1244954ed246d801
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-23 02:13:41 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,11 +1,18 @@
-function solution(my_string) {
-    let answer = [];
-    for(let i = 0; i < my_string.length; i++){
-      let num = my_string[i]
-      if(!isNaN(num)){
-          answer.push(Number(num))
-      }
-    }
-    answer.sort((a,b)=> a-b)
-    return answer;
-}
+// function solution(my_string) {
+//     let answer = [];
+//     for(let i = 0; i < my_string.length; i++){
+//       let num = my_string[i]
+//       if(!isNaN(num)){
+//           answer.push(Number(num))
+//       }
+//     }
+//     answer.sort((a,b)=> a-b)
+//     return answer;
+// }
+
+const solution = (my_string) => my_string
+.split('')
+.filter((n)=> !isNaN(n)) // 숫자이면 true
+.map((n)=> n * 1) // 문자열을 숫자로 변환하는 트릭
+.sort((a, b)=> a - b)
+ 
