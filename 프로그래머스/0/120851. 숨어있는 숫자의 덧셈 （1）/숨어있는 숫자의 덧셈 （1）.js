a86b339b4db8a5c09fcb052332aa89c49956e450
yuna <65221477+yuna-c@users.noreply.github.com>
2025-06-25 11:18:21 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,9 +1,11 @@
-function solution(my_string) {
-    let answer = 0;
-    for(let char of my_string){
-        if(!isNaN(char) && char !== ''){
-            answer += Number(char)
-        }
-    }
-    return answer;
-}
+// function solution(my_string) {
+//     let answer = 0;
+//     for(let char of my_string){
+//         if(!isNaN(char) && char !== ''){
+//             answer += Number(char)
+//         }
+//     }
+//     return answer;
+// }
+
+const solution = (my_string) => my_string.split('').filter((v)=> !isNaN(v)).map(Number).reduce((acc, cur) => acc + cur, 0)
