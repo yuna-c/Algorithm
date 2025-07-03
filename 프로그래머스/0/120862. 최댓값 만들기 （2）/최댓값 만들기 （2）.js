cc7439ef1695aba140fa901ef8d5de3931ca078f
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-03 17:41:20 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,15 +1,20 @@
+// function solution(numbers) {
+//     let maxProduct = numbers[0] * numbers[1]; // 초기화
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         for (let j = i + 1; j < numbers.length; j++) {
+//             const product = numbers[i] * numbers[j];
+//             if (product > maxProduct) {
+//                 maxProduct = product;
+//             }
+//         }
+//     }
+//     return maxProduct;
+// }
+
 function solution(numbers) {
-    let maxProduct = numbers[0] * numbers[1];
-
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            const product = numbers[i] * numbers[j];
-            if (product > maxProduct) {
-                maxProduct = product;
-            }
-        }
-    }
-
-    console.log(maxProduct);
-    return maxProduct;
+    const sortNum = numbers.sort((a,b)=> a-b);
+    const min = sortNum[0] * sortNum[1]
+    const max = sortNum[sortNum.length -1] * sortNum[sortNum.length -2] 
+    return Math.max(min, max)
 }
