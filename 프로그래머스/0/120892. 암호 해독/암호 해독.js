4844daff79f32a60bc3b663a03bca11b42f521fb
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-04 13:35:09 +0900

    [level 0] Title: 암호 해독, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,7 +1,11 @@
+// function solution(cipher, code) {
+//     let answer = '';
+//     for(let i = code - 1; i < cipher.length; i += code){
+//       answer += cipher[i]
+//     }
+//     return answer;
+// }
+
 function solution(cipher, code) {
-    let answer = '';
-    for(let i = code - 1; i < cipher.length; i += code){
-      answer += cipher[i]
-    }
-    return answer;
+    return cipher.split('').filter((_, i)=> (i + 1) % code === 0).join('')
 }
