1fe57a7c5c27cb1e99b970ae6e1c7184ba171c39
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-05 18:30:10 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,11 +1,17 @@
+// function solution(my_string) {
+//     let answer = '';
+//     for(let i = 0; i < my_string.length; i++){
+//         if(my_string[i] === my_string[i].toUpperCase()){
+//             answer += my_string[i].toLowerCase()
+//         } else {
+//             answer += my_string[i].toUpperCase()
+//         }
+//     }
+//     return answer;
+// }
+
+
 function solution(my_string) {
-    let answer = '';
-    for(let i = 0; i < my_string.length; i++){
-        if(my_string[i] === my_string[i].toUpperCase()){
-            answer += my_string[i].toLowerCase()
-        } else {
-            answer += my_string[i].toUpperCase()
-        }
-    }
-    return answer;
+    return my_string.split('').map((i)=> i === 
+                                   i.toUpperCase() ? i.toLowerCase() : i.toUpperCase()).join('');
 }
