68a3c8a94b53bc1e22fe8ecc3bca62782740c652
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-14 10:32:32 +0900

    [level 0] Title: 약수 구하기, Time: 1.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -6,9 +6,16 @@
 //     return answer;
 // }
 
+// const solution = (n) => {
+//     return Array(n) // 길이 n의 배열 생성
+//         .fill(0) // 모든 요소를 0으로 채움
+//         .map((x, i) => x + i + 1) // 1부터 n까지의 숫자 배열 생성
+//         .filter((d) => n % d === 0); // n을 d로 나누었을 때 나머지가 0인 값만 필터링 (약수만 남김)
+// }
+
 const solution = (n) => {
-    return Array(n) // 길이 n의 배열 생성
-        .fill(0) // 모든 요소를 0으로 채움
-        .map((x, i) => x + i + 1) // 1부터 n까지의 숫자 배열 생성
-        .filter((d) => n % d === 0); // n을 d로 나누었을 때 나머지가 0인 값만 필터링 (약수만 남김)
+    return Array(n)              // 길이 n짜리 배열 생성
+        .fill(1)                 // 모든 요소를 1로 채움 (사실 어떤 값이든 상관없음)
+        .map((_, i) => i + 1)    // 1부터 n까지 숫자 배열 생성
+        .filter((d) => n % d === 0); // n의 약수만 필터링
 }
