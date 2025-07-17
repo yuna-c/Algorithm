1adf0d248b4d9dc4e70971bfba3d724f5464776c
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-18 03:22:51 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,13 +1,20 @@
-function solution(array) {
-    let max = array[0]
-    let index = 0
+// function solution(array) {
+//     let max = array[0]
+//     let index = 0
     
-    for(let i = 1; i < array.length; i++){
-        if(array[i] > max){
-            max = array[i] // 더 큰 수 찾으면 갱신
-            index = i // 인덱스도 갱신
-        }
-    }
+//     for(let i = 1; i < array.length; i++){
+//         if(array[i] > max){
+//             max = array[i] // 더 큰 수 찾으면 갱신
+//             index = i // 인덱스도 갱신
+//         }
+//     }
+    
+//     return [max,index];
+// }
+
+function solution(array) {
+   const max = Math.max(...array) // 배열에서 가장 큰 수를 찾음
+   const index = array.indexOf(max) // 그 수의 인덱스를 찾음
     
     return [max,index];
 }
