9bb213328ce20cc34191b35e96522bc72d7377b0
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-19 07:49:42 +0900

    [level 0] Title: 숫자 찾기, Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.js
@@ -1,4 +1,5 @@
 function solution(num, k) {
-    let index = num.toString().indexOf(k.toString())
+    // let index = num.toString().indexOf(k.toString())
+    let index = num.toString().split('').map((e)=> Number(e)).indexOf(k)
     return index === -1 ? -1 : index + 1 ;
 }
