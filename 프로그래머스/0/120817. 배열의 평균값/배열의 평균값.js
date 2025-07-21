7990fdd671efcf9b65b37b9df8d76e8e4155c607
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-21 18:00:04 +0900

    [level 0] Title: 배열의 평균값, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,10 +1,12 @@
-function solution(numbers) {
-    // const answer = numbers.reduce((acc, cur) => acc + cur, 0);
-    // return answer / numbers.length;
-    
+// const answer = numbers.reduce((acc, cur) => acc + cur, 0);
+// return answer / numbers.length;
+
+function solution(n) {
     let answer = 0;
-    for(let i = 0; i < numbers.length; i++){
-        answer += numbers[i] / numbers.length;
+    for(let i = 0; i < n.length; i++){
+         answer += n[i] / n.length
+        // n[i] n의 i번째 요소를 배열의 길이로 나누고 누적덧셈
     }
+        
     return answer;
 }
