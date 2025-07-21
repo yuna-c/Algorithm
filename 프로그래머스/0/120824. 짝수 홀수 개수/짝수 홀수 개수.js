afde0a33c1d70b6df155b382a0933f346b367ab3
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-21 18:06:40 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,11 +1,10 @@
 function solution(num_list) {
     let answer = [];
-    let numArr = [];
- 
+    let newArr = [];
     for(let i = 0; i < num_list.length; i++){
-        // 짝수로 나누었을 때의 값을 numArr에 넣어준다.
-        if(num_list[i] % 2 === 0) numArr.push(i);     
+        if(num_list[i] % 2 === 0) newArr.push(i)     
     }
-    // console.log(numArr.length, num_list.length-numArr.length); 짝수, 홀수
-    return answer = [numArr.length, num_list.length-numArr.length];
+    // console.log(newArr.length, num_list.length - newArr.length)
+    
+    return answer = [newArr.length, num_list.length - newArr.length]
 }
