538b5b6fa942be015eb67f908e55e2a8307c8e47
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-21 17:38:28 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,7 +1,7 @@
-const solution =(num1, num2) => answer = Math.floor((num1 / num2) * 1000);
-
-// function solution(num1, num2) {
-//     let answer = 0;
-//     answer = Math.floor((num1 / num2) * 1000);
-//     return answer;
-// }
+// 1.num1/num2 나눈다
+// 2.*1000을 곱한다
+// 3.소숫점을 버린다
+function solution(num1, num2) {
+    const answer = Math.floor((num1/num2)* 1000)
+    return answer;
+}
