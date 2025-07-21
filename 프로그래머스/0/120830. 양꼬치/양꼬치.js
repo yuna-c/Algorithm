705775854a281ae8ec0de44b4ddc82cca2fde9b7
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-21 17:51:46 +0900

    [level 0] Title: 양꼬치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -4,7 +4,14 @@
 //     return bill;
 // }
 
-function solution(n, k) {
-    k -= Math.floor(n / 10);
-    return (n * 12000) + (k * 2000);
-}
+// function solution(n, k) {
+//     k -= Math.floor(n / 10);
+//     return (n * 12000) + (k * 2000);
+// }
+
+// 1.10인분 서비스 1개
+function solution (n, k){
+    let free = Math.floor(n/10)
+    let cost = (n * 12000) + ((k - free) * 2000)
+    return cost
+} 
