2fc33c6aafc83b40771aee6065193957033cb600
yuna <65221477+yuna-c@users.noreply.github.com>
2025-07-23 14:51:35 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,7 +1,18 @@
+// function solution(age) {
+//     const alpha = ['a','b','c','d','e','f','g','h','i','j']
+//     // 숫자는 공백이 없어
+//     const result = age.toString().split('').map((n)=> alpha[n]).join('')
+//     console.log(result)
+//     return result;
+// }
+
 function solution(age) {
     const alpha = ['a','b','c','d','e','f','g','h','i','j']
-    // 숫자는 공백이 없어
-    const result = age.toString().split('').map((n)=> alpha[n]).join('')
-    console.log(result)
+    let result = '' // 변수 담을 공간은 let
+    age = age.toString()
+    
+    for(let i = 0; i < age.length; i++){
+       result += alpha[age[i]]
+    }
     return result;
 }
